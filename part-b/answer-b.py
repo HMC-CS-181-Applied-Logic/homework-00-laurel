1323050a96317a51815ae459d2495002abfcb72d
LaurelMonte <laurelmonte@gmail.com>
2020-01-22 19:32:17 +0000

    partb

--- part-b/answer-b.py
+++ part-b/answer-b.py
@@ -9,4 +9,13 @@ C = BoolVar('C')
 # and the .format() mathod to output the
 # following expression:
 
-# (((A => B) & (B => C)) => (A => C))
+f1 = Implies(A, B)
+f2 = Implies(B, C)
+f3 = And(f1, f2)
+f4 = Implies(A, C)
+f5 = Implies(f3, f4)
+
+#if we wanted to print the logic code we could also call: print(f5) 
+print(f5.format())
+print()
+# prints (((A => B) & (B => C)) => (A => C))
